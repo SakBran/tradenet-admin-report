0f4dcef915c78717911ca4581e0a64b6a3683712
Sao Bran Aung <saobran.aung61@gmail.com>
2026-05-30 15:24:59 +0630

    fix: update indexd for vw_ImportLicenceItemTotalByCurrency

--- StoredProcedureMigrations/Views/vw_ImportLicenceItemTotalByCurrency.sql
+++ StoredProcedureMigrations/Views/vw_ImportLicenceItemTotalByCurrency.sql
@@ -23,3 +23,10 @@ GROUP BY
     ili.ImportLicenceId,
     ili.CurrencyId;
 GO
+
+-- Materialize the view so the report join is an index seek instead of a
+-- full re-aggregation of ImportLicenceItem on every request.
+-- The unique clustered index must be on the GROUP BY columns.
+CREATE UNIQUE CLUSTERED INDEX IX_vw_ImportLicenceItemTotalByCurrency
+    ON dbo.vw_ImportLicenceItemTotalByCurrency (ImportLicenceId, CurrencyId);
+GO
