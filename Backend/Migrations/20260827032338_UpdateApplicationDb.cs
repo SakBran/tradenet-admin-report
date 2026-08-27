ca730adf543ecab9e487218780cf4b24d7105b1a
Sak Bran Aung MOC VM <saobran.aung61@gmail.com>
2026-08-27 10:09:43 +0630

    feat: add ImportLicence table and update ApplicationDbContext model snapshot

---
+++ Backend/Migrations/20260827032338_UpdateApplicationDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateApplicationDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImportLicence",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalCount = table.Column<int>(type: "int", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    LicenceDate = table.Column<DateTime>(type: "date", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImportLicence", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImportLicence_LicenceDate",
+                table: "ImportLicence",
+                column: "LicenceDate",
+                unique: true,
+                filter: "[LicenceDate] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImportLicence");
+        }
+    }
+}
